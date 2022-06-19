84670c0f2346864794f9ce553a4cc83e07f7c737
IvanSeagull <ivan.seagull.k@gmail.com>
2022-06-19 22:21:32 +0100

    Added skeleton while loading

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import React from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
+import Skeleton from './components/PizzaBlock/Skeleton';
 import Sort from './components/Sort';
 import './scss/app.scss';
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   const getPizzas = () => {
     fetch('/api/pizzas.json')
       .then((res) => res.json())
-      .then((data) => setItems(data));
+      .then((data) => setItems(data))
+      .finally(() => setIsLoading(false));
   };
 
   React.useEffect(() => {
@@ -30,9 +33,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items?.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? new Array(10).fill(0).map((_, index) => <Skeleton key={index} />)
+              : items?.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const Skeleton = () => (
+  <ContentLoader
+    className="pizza-block"
+    speed={2}
+    // width={280}
+    height={460}
+    viewBox="0 0 280 460"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb">
+    <circle cx="140" cy="128" r="128" />
+    <rect x="0" y="272" rx="10" ry="10" width="280" height="35" />
+    <rect x="0" y="315" rx="10" ry="10" width="280" height="88" />
+    <rect x="0" y="411" rx="10" ry="10" width="95" height="30" />
+    <rect x="130" y="410" rx="10" ry="10" width="150" height="30" />
+  </ContentLoader>
+);
+
+export default Skeleton;
