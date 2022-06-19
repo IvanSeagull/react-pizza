f66cd153a6c406ab282765a8f64448d6551d6b3a
IvanSeagull <ivan.seagull.k@gmail.com>
2022-06-19 16:41:39 +0100

    Added different pizzas

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,21 +1,33 @@
-const PizzaBlock = ({ title, price }) => {
+import React from 'react';
+
+const PizzaBlock = ({ title, price, imageUrl, sizes, types }) => {
+  const typeNames = ['тонкое', 'традиционное'];
+  const [activeSizeIndex, setActiveSizeIndex] = React.useState(0);
+  const [activeTypeIndex, setActiveTypeIndex] = React.useState(0);
   return (
     <div className="pizza-block">
-      <img
-        className="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-        alt="Pizza"
-      />
+      <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
-          <li className="active">тонкое</li>
-          <li>традиционное</li>
+          {types?.map((type, index) => (
+            <li
+              key={index}
+              onClick={() => setActiveTypeIndex(index)}
+              className={activeTypeIndex === index ? 'active' : ''}>
+              {typeNames[type]}
+            </li>
+          ))}
         </ul>
         <ul>
-          <li className="active">26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes?.map((size, index) => (
+            <li
+              onClick={() => setActiveSizeIndex(index)}
+              className={activeSizeIndex === index && 'active'}
+              key={index}>
+              {size} см.
+            </li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
